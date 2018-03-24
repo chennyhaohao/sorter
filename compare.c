d82fdd2799d21f5c8cc96616232e2ad99d3aa882
Chen <chennyhaohao@gmail.com>
2018-03-24 19:08:57 +0400

    more documentation

--- compare.c
+++ compare.c
@@ -1,6 +1,8 @@
 #include <string.h>
 #include "./record.c"
 
+//Functions that compare 2 records based on different parameters
+//returns >0 if rec1 greater than rec2, ==0 if the two are equal, and <0 if rec1 smaller than rec2
 
 int cmp_by_id(const void * rec1, const void * rec2) {
 	return ((tax_rec*)rec1)->id - ((tax_rec*)rec2)->id;
